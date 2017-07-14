c2f3278cb963f56a1624811c77eba985f327a0a3
martin.bobcik <martin.bobcik@solarwinds.com>
2017-07-14 14:29:11 +0200

    Revert "run4 fail"
    This reverts commit c237390a8e63831d06e2cdf18370109bc990340f.

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -5,40 +5,10 @@ using System.Text;
 
 namespace C_sharp
 {
-
-    class Game
-    {
-
-        public List<bool> world;
-
-        public Game(int size)
-        {
-            world = new List<bool>();
-        }
-
-        public int getEnvelope(int i, int j, bool[,] world)
-        {
-            return;// world[i - 1, j - 1] + world[i - 1, j] + world[i - 1, j + 1] + world[i, j - 1] + world[i - 1, j - 1];
-        }
-
-        public Game Transition(Game last)
-        {
-            Game next = new Game();
-
-            
-
-            return next;
-        }
-    }
-
     class Program
     {
-
-        
-
         static void Main(string[] args)
         {
-
         }
     }
 }
